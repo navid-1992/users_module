056ca1df06b4fc2500716d3a19c296bd1571301c
cashgatetechnologyinformation <naveedanjum42@gmail.com>
2025-03-26 16:04:33 +0400

    Frontend

---
+++ vuejs/src/components/HelloWorld.vue
@@ -0,0 +1,44 @@
+<template>
+  <table>
+    <thead>
+      <tr>
+        <th>Name</th>
+        <th>Email</th>
+      </tr>
+    </thead>
+    <tbody v-if="users.length">
+      <tr v-for="user in users" :key="user.id">
+        <td>{{ user.name }}</td>
+        <td>{{ user.email }}</td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  name: 'HelloWorld',
+  data(){
+    return {
+      users:[]
+    }
+  },
+  props: {
+    msg: String
+  },
+
+  created(){
+    this.fetchUsers()
+  },
+
+  methods:{
+    async fetchUsers(){
+      let users = await axios.get('https://usermodules.test/api/users')
+      this.users = users.data
+    }
+  }
+}
+</script>
+
+
